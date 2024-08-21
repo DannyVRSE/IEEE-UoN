71948ed9feed603f063f2efb5db006af27c3bba1
DannyVRSE <mshindiouma1024@gmail.com>
2024-08-21 08:47:47 +0300

    fix (pwa manifest)

--- src/client/pages/Home.jsx
+++ src/client/pages/Home.jsx
@@ -28,11 +28,11 @@ const Home = () => {
 
   return (
     <>
-    {/*background video */}
+      {/*background video */}
       <video autoPlay loop muted id="tech-background-video">
         <source src={backgroundVideo} type="video/mp4" />
       </video>
-      
+
       <div className="container">
         <div className="inner-div-1">
           {user && <div>{loading && <div className="loader-2"></div>}<p>{user.name}</p></div>}
@@ -41,10 +41,10 @@ const Home = () => {
 
           <button onClick={() => { navigate("/login") }} className={btnClass} style={style}>{text}</button>
 
-          <button onClick={() => { navigate("/registration") }} role="button"  disabled={user} className="btn btn-info home-btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill-add" viewBox="0 0 16 16">
+          <button onClick={() => { navigate("/registration") }} role="button" disabled={user} className="btn btn-info home-btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill-add" viewBox="0 0 16 16">
             <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
             <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4" />
-          </svg> {user?"Joined":"Join"}</button>
+          </svg> {user ? "Joined" : "Join"}</button>
 
           <button onClick={() => { navigate("society/ieee_uon") }} className="btn btn-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-info-circle-fill" viewBox="0 0 16 16">
             <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2" />
--- vite.config.js
+++ vite.config.js
@@ -9,25 +9,30 @@ export default defineConfig({
     injectRegister: false,
 
     manifest: {
-      name: 'ieee_uon',
+      name: 'ieee uon',
       short_name: 'ieee',
-      description: '"society managemement app"',
+      description: '"ieee uon society app"',
       theme_color: '#000000',
 
       icons: [{
-        src: 'pwa-64x64.png',
-        sizes: '64x64',
-        type: 'image/png',
-      }, {
-        src: 'pwa-192x192.png',
+        src: 'android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       }, {
-        src: 'pwa-512x512.png',
+        src: 'android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
-      }, {
-        src: 'maskable-icon-512x512.png',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      }
+        ,
+
+      {
+        src: 'maskable_icon_x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
