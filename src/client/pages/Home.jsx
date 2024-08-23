fb2ab93a62c1ef7301026d15a5733a4bbffc4167
DannyVRSE <mshindiouma1024@gmail.com>
2024-08-23 20:29:23 +0300

    refactor home page

--- src/client/pages/Home.jsx
+++ src/client/pages/Home.jsx
@@ -1,29 +1,20 @@
 import PWABadge from "../components/PWABadge"
 import Footer from "../components/Footer"
 import { useAuth } from "../hooks/AuthContext"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import backgroundVideo from "../assets/videos/tech_background.mp4"
 import { useNavigate } from "react-router-dom"
 
 const Home = () => {
-  const navigate = useNavigate();
-  const { user, getUser, loading } = useAuth();
-  const [text, setText] = useState("Log in");
-  const [btnClass, setBtnClass] = useState("btn btn-primary home-btn");
-  const [style, setStyle] = useState({})
+
+  const navigate = useNavigate();//navigate to other pages
+
+  const { user, getUser, loading } = useAuth();//get user and loading state from AuthContext
 
   //get use on login
+
   useEffect(() => {
     getUser();
-    if (user) {
-      //console.log(user)
-      //change text to logged in
-      setText("Logged in")
-      //modify button to disabled
-      setBtnClass("btn btn-success disabled home-btn")
-      //remove pointer
-      setStyle({ pointerEvents: "none" })
-    }
   }, [])
 
   return (
@@ -34,12 +25,13 @@ const Home = () => {
       </video>
 
       <div className="container">
+
         <div className="inner-div-1">
           {user && <div>{loading && <div className="loader"></div>}<p>{user.name}</p></div>}
           <h1>Welcome To IEEE UoN Student Branch</h1>
           <br />
 
-          <button onClick={() => { navigate("/login") }} className={btnClass} style={style}>{text}</button>
+          <button onClick={() => { navigate("/login") }} style={user ? { pointerEvents: "none" } : {}} className={user ? "btn btn-success disabled home-btn" : "btn btn-primary home-btn"}>{user ? "Logged In" : "Log In"}</button>
 
           <button onClick={() => { navigate("/registration") }} role="button" disabled={user} className="btn btn-info home-btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill-add" viewBox="0 0 16 16">
             <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
@@ -49,8 +41,10 @@ const Home = () => {
           <button onClick={() => { navigate("society/ieee_uon") }} className="btn btn-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-info-circle-fill" viewBox="0 0 16 16">
             <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2" />
           </svg> About</button>
+
           <PWABadge />
           <Footer />
+
         </div>
       </div>
     </>
