cdf07967e7d1001b67b1f5621c1985e601ca5ac3
DannyVRSE <mshindiouma1024@gmail.com>
2024-08-29 12:03:02 +0300

    modifify logos to uon specific

--- src/client/components/Layout.jsx
+++ src/client/components/Layout.jsx
@@ -1,11 +1,11 @@
 import { Outlet, Link } from 'react-router-dom';
-import logo from "../assets/images/ieee_logo.png"
+import logo from "../assets/images/ieee_uon_logo.png"
 const Layout = () => {
   return (
     <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img src={logo} alt="ieee_logo" width="80"/></a>
+                    <a className="navbar-brand" href="https://edu.ieee.org/ke-uonbi/" target="_blank"><img src={logo} alt="ieee_logo" height="45"/></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -13,7 +13,7 @@ const Layout = () => {
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav ms-auto">
                                 <li className="nav-item"><Link to="/" className="nav-link active">Home</Link></li>
-                                <li className="nav-item"><Link to="/community" className="nav-link">Community</Link></li>
+                                <li className="nav-item"><Link to="/community" className="nav-link">Societies</Link></li>
                                 <li className="nav-item"><Link to="/account" className="nav-link">Profile</Link></li>
                                 <li className="nav-item"><Link to="/about" className="nav-link">About IEEE</Link></li>
                             </ul>
--- src/client/pages/Home.jsx
+++ src/client/pages/Home.jsx
@@ -55,7 +55,7 @@ const Home = () => {
           <br />
 
           <div>
-            <h5><a href="/community" >Explore the community <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+            <h5><a href="/community" >Explore Societies <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
               <path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5" />
               <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z" />
             </svg></a></h5>
